f3658c2797d6866c2d440691b5d58b8d41a97be0
Ruveyda Kisla <thisisruveyda@gmail.com>
2025-01-13 17:06:46 +0330

    feat: add disconnect

--- src/ConnectButton.tsx
+++ src/ConnectButton.tsx
@@ -1,27 +1,49 @@
 import { useBlux } from "blux";
 
 const ConnectButton = () => {
-  const { connect } = useBlux();
+  const { connect, user, disconnect } = useBlux();
 
   const handleClick = async () => {
     await connect();
+    console.log(user());
+  };
+
+  const handleDisconnect = async () => {
+    await disconnect();
+    console.log(user());
   };
 
   return (
-    <button
-      onClick={handleClick}
-      style={{
-        background: "#0d1292",
-        padding: "10px",
-        border: "1px solid transparent",
-        borderRadius: "12px",
-        color: "white",
-        fontSize: "16px",
-        fontWeight: "bold",
-      }}
-    >
-      Connect wallet
-    </button>
+    <>
+      <button
+        onClick={handleClick}
+        style={{
+          background: "#0d1292",
+          padding: "10px",
+          border: "1px solid transparent",
+          borderRadius: "12px",
+          color: "white",
+          fontSize: "16px",
+          fontWeight: "bold",
+        }}
+      >
+        Connect wallet
+      </button>
+      <button
+        onClick={handleDisconnect}
+        style={{
+          background: "#878787",
+          padding: "10px",
+          border: "1px solid transparent",
+          borderRadius: "12px",
+          color: "white",
+          fontSize: "16px",
+          fontWeight: "bold",
+        }}
+      >
+        disconnect wallet
+      </button>
+    </>
   );
 };
 
