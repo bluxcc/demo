27cf3450530ff4827464e5bb32299e73a124d1d3
Ruveyda Kisla <thisisruveyda@gmail.com>
2025-01-15 02:55:12 +0330

    test: get address in console

--- src/ConnectButton.tsx
+++ src/ConnectButton.tsx
@@ -1,33 +1,48 @@
 import { useBlux } from "blux";
+import { useEffect, useState } from "react";
+import { shortenAddress } from "./utils/shortenAddress";
 
 const ConnectButton = () => {
   const { connect, user, disconnect } = useBlux();
+  const [userAddress, setUserAddress] = useState("");
+  const { address } = user();
+
+  useEffect(() => {
+    setUserAddress(address);
+    console.log(address);
+  }, [address]);
 
   const handleClick = async () => {
     await connect();
-    console.log(user());
   };
 
   const handleDisconnect = async () => {
     await disconnect();
-    console.log(user());
   };
 
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100dvh",
+      }}
+    >
       <button
         onClick={handleClick}
         style={{
           background: "#0d1292",
-          padding: "10px",
+          padding: "10px 16px",
           border: "1px solid transparent",
           borderRadius: "12px",
           color: "white",
           fontSize: "16px",
           fontWeight: "bold",
+          margin: "0 10px",
         }}
       >
-        Connect wallet
+        {userAddress ? shortenAddress(userAddress, 4) : "Connect wallet"}
       </button>
       <button
         onClick={handleDisconnect}
@@ -43,7 +58,7 @@ const ConnectButton = () => {
       >
         disconnect wallet
       </button>
-    </>
+    </div>
   );
 };
 
---
+++ src/utils/shortenAddress.ts
@@ -0,0 +1,6 @@
+export const shortenAddress = (address: string, numChars = 8) => {
+  const shortenedAddress =
+    address.slice(0, numChars) + "..." + address.slice(-numChars);
+
+  return shortenedAddress;
+};
