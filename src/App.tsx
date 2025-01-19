fbd585610493f03c9837297fd3c36cb54d9e0451
Ruveyda Kisla <thisisruveyda@gmail.com>
2025-01-20 00:09:47 +0330

    fix: modal is centered in the parent now

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BluxProvider } from "blux";
 
 import "./style/index.css";
+// import "../../blux/dist/index.esm.css";
 
 import Header from "./components/header";
 import ConnectButton from "./components/ConnectButton";
@@ -10,33 +11,33 @@ import TabsContainer from "./containers/tabsContainer";
 
 function App() {
   return (
-    <BluxProvider
-      config={{
-        networkPassphrase: "Test SDF Network ; September 2015",
-        appName: "my example app",
-      }}
-    >
-      <div className="flex-col h-screen w-screen">
-        <Header />
-        <div className="h-[calc(100vh-72px)] flex">
-          <TabsContainer />
-          <div className="w-full relative">
-            <div className="absolute py-6 flex-col flex justify-between items-center inset-0 h-full w-full bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
-              <div />
-              <div>
+    <div className="flex-col h-screen w-screen">
+      <Header />
+      <div className="h-[calc(100vh-72px)] flex">
+        <TabsContainer />
+        <div className="w-full relative h-full overflow-hidden">
+          <div className="absolute py-6 flex-col flex justify-between items-center inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+            <div />
+            <div className="relative w-full center">
+              <BluxProvider
+                config={{
+                  networkPassphrase: "Test SDF Network ; September 2015",
+                  appName: "my example app",
+                }}
+              >
                 <ConnectButton />
-              </div>
-              <div className="">
-                <button className="inline-flex gap-2 justify-center items-center text-primary border-primary border-2 border-dashed h-12 w-[120px] ">
-                  <img src={redo} alt="redo" />
-                  Reset
-                </button>
-              </div>
+              </BluxProvider>
+            </div>
+            <div className="">
+              <button className="inline-flex gap-2 justify-center items-center text-primary border-primary border-2 border-dashed h-12 w-[120px] ">
+                <img src={redo} alt="redo" />
+                Reset
+              </button>
             </div>
           </div>
         </div>
       </div>
-    </BluxProvider>
+    </div>
   );
 }
 
