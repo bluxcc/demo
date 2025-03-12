38b4da9bb05c14c0861761e8d9494e968b71d3fb
Ruveyda Kisla <thisisruveyda@gmail.com>
2025-03-12 18:57:11 +0330

    feat: add code highlighting feature using prism-react-renderer

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,11 @@
 import logo from "/images/logo.svg";
 import barChart from "/images/barChart.svg";
 
-const Header = () => {
+type HeaderProps = {
+  onOpenCode: () => void;
+};
+
+const Header = ({ onOpenCode }: HeaderProps) => {
   return (
     <div className="w-full h-[72px] border border-b-lightPurple between px-4 font-jetbrains">
       <div>
@@ -11,7 +15,12 @@ const Header = () => {
         <button className="center border border-primary w-10 h-10">
           <img src={barChart} alt="barChart" />
         </button>
-        <button className="bg-primary w-[120px] h-10 text-white">Code</button>
+        <button
+          className="bg-primary w-[120px] h-10 text-white"
+          onClick={onOpenCode}
+        >
+          Code
+        </button>
       </div>
     </div>
   );
